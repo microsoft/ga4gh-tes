53f2611bb65ebc07099c607d5549332947755415
yuliadub <yulia.dubinina@gmail.com>
2023-01-17 16:21:16 -0800

    not having success, switching the logic up a bit

--- src/TesApi.Web/compliance-test/entrypoint.sh
+++ src/TesApi.Web/compliance-test/entrypoint.sh
@@ -1,6 +1,2 @@
 #!/bin/sh
-
-ls
-cd /app/tes-compliance-suite/
-
-tes-compliance-suite report --server https://localhost/ --tag all --output_path /app/results
+tes-compliance-suite report --server https://localhost/ --tag all --output_path results
