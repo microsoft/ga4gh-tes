b8447e85a5398d9bd419ccc87b9887e6613a4545
Matt McLoughlin <mattmcl@microsoft.com>
2023-01-22 19:04:08 -0800

    set default options values

--- src/Tes/Models/PostgreSqlOptions.cs
+++ src/Tes/Models/PostgreSqlOptions.cs
@@ -8,11 +8,11 @@
         public const string PostgreSqlAccount = "PostgreSql";
 
         public string PostgreSqlServerName { get; set; }
-        public string PostgreSqlTesDatabaseName { get; set; }
-        public string PostgreSqlTesDatabasePort { get; set; }
+        public string PostgreSqlServerNameSuffix { get; set; } = ".postgres.database.azure.com";
+        public string PostgreSqlTesDatabaseName { get; set; } = "tes_db";
+        public string PostgreSqlTesDatabasePort { get; set; } = "5432";
         public string PostgreSqlTesUserLogin { get; set; }
         public string PostgreSqlTesUserPassword { get; set; }
-        public string PostgreSqlServerNameSuffix { get; set; } = ".postgres.database.azure.com";
         public string PostgreSqlTesSslMode { get; set; } = "VerifyFull";
     }
 }
