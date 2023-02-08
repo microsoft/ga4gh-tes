3fa3d00da797d919d144d25cf48ea1b59a464b4e
Matt McLoughlin <mattmcl@microsoft.com>
2023-02-07 18:23:54 -0800

    remove mains

--- src/CommonUtilities.Tests/Base32Tests.cs
+++ src/CommonUtilities.Tests/Base32Tests.cs
@@ -18,7 +18,5 @@ namespace CommonUtilities.Tests
         [DataRow("foobar", "MZXW6YTBOI======")]
         public void ValidateConvertToBase32(string data, string expected)
             => Assert.AreEqual(expected, Base32.ConvertToBase32(Encoding.UTF8.GetBytes(data)));
-
-        static void Main() { }
     }
 }
