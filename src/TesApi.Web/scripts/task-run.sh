b56ed12f4e52c3f1b51d7b51ac6edf9da6968c7c
Blair L Murri <BMurri@users.noreply.github.com>
2023-03-17 15:10:14 -0700

    More agressive compute node cleaning

--- src/TesApi.Web/scripts/task-run.sh
+++ src/TesApi.Web/scripts/task-run.sh
@@ -1,6 +1,5 @@
-sudo echo docker rmi {TaskExecutor} -f > clean-executor.sh
-sudo chmod a+x clean-executor.sh
-sudo find $AZ_BATCH_NODE_ROOT_DIR/workitems -maxdepth 5 -path $(dirname $(dirname $(dirname $PWD))) -prune -o -type f -name clean-executor.sh -execdir '{}' \; || :
-sudo find $AZ_BATCH_NODE_ROOT_DIR/workitems -maxdepth 5 -path $(dirname $(dirname $(dirname $PWD))) -prune -o -type d -name cromwell-executions -execdir rm -fdr '{}' \; || :
+sudo docker rm $(docker ps -aq) || :
+sudo docker image prune -a || :
+sudo find $AZ_BATCH_NODE_ROOT_DIR/workitems -maxdepth 5 -path $(dirname $(dirname $(dirname $PWD))) -prune -o -type d -name wd -execdir rm -fdr '{}' \; || :
 sudo docker system prune --volumes -f || :
 /bin/sh {BatchScriptPath}
