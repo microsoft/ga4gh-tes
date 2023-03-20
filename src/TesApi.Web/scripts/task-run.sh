e7080b8614db14ceddf366457d3475c7a986dc30
Blair L Murri <BMurri@users.noreply.github.com>
2023-03-20 13:00:12 -0700

    Always prune containers

--- src/TesApi.Web/scripts/task-run.sh
+++ src/TesApi.Web/scripts/task-run.sh
@@ -2,5 +2,6 @@ sudo echo -n 'if [ "$1" != "{TaskExecutor}" ]; then docker rmi {TaskExecutor} -f
 sudo echo -n ' rm -fdr wd/{ExecutionPathPrefix} || :' >> ../clean-executor.sh
 sudo chmod a+x ../clean-executor.sh
 sudo find $AZ_BATCH_NODE_ROOT_DIR/workitems -maxdepth 5 -path $(dirname $(dirname $(dirname $PWD))) -prune -o -type f -name clean-executor.sh -execdir '{}' {TaskExecutor} \; || :
+sudo docker container prune -f || :
 sudo docker system prune --volumes -f || :
 /bin/sh {BatchScriptPath}
