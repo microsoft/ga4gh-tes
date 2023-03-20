40fc79ffbbe5c9d79f4ff93cdda569ff7ce3e64d
Matt McLoughlin <mattmcl@microsoft.com>
2023-03-20 11:25:19 -0700

    add comments

--- src/TesApi.Web/TesTaskCache.cs
+++ src/TesApi.Web/TesTaskCache.cs
@@ -4,6 +4,10 @@ using Tes.Repository;
 
 namespace TesApi.Web
 {
+    /// <summary>
+    /// A wrapper for LazyCache.IAppCache
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
     public class TesRepositoryLazyCache<T> : ICache<T> where T : class
     {
         /// <summary>
@@ -12,37 +16,47 @@ namespace TesApi.Web
         /// 
         private static TimeSpan defaultItemExpiration = TimeSpan.FromDays(30);
 
-        private IAppCache cache;
+        private readonly IAppCache cache;
 
+        /// <summary>
+        /// Default constructor expecting the singleton LazyCache.IAppCache
+        /// </summary>
+        /// <param name="appCache"></param>
         public TesRepositoryLazyCache(IAppCache appCache)
         {
             cache = appCache;
         }
 
+        /// <inheritdoc/>
         public int MaxCount { get => throw new System.NotSupportedException(); set => throw new System.NotSupportedException(); }
 
+        /// <inheritdoc/>
         public int Count()
         {
             throw new System.NotSupportedException();
         }
 
+        /// <inheritdoc/>
         public bool TryAdd(string key, T task)
         {
             cache.Add(key, task, defaultItemExpiration);
             return true;
         }
 
+        /// <inheritdoc/>
         public bool TryGetValue(string key, out T task)
         {
             return cache.TryGetValue(key, out task);
         }
 
+        /// <inheritdoc/>
         public bool TryRemove(string key)
         {
             cache.Remove(key);
             return true;
         }
 
+        /// <inheritdoc/>
         public bool TryUpdate(string key, T task)
         {
             return TryAdd(key, task);
