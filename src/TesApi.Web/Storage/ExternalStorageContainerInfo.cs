8780a68290dd7760e1cc79c04c250ebe224d8dad
Adina Shanholtz <ashanhol@gmail.com>
2023-04-17 15:42:47 -0700

    discard urlsegments, add to public externalstoragecontainerinfo

--- src/TesApi.Web/Storage/ExternalStorageContainerInfo.cs
+++ src/TesApi.Web/Storage/ExternalStorageContainerInfo.cs
@@ -8,6 +8,8 @@ namespace TesApi.Web.Storage;
 /// </summary>
 public class ExternalStorageContainerInfo
 {
+    private string sasToken;
+
     /// <summary>
     /// Account name
     /// </summary>
@@ -23,5 +25,9 @@ public class ExternalStorageContainerInfo
     /// <summary>
     /// Sas Token
     /// </summary>
-    public string SasToken { get; set; }
+    public string SasToken
+    {
+        get => sasToken;
+        set => sasToken = value?.TrimStart('?');
+    }
 }
--- src/TesApi.Web/StorageAccountUrlSegments.cs
+++ src/TesApi.Web/StorageAccountUrlSegments.cs
@@ -22,8 +22,6 @@ namespace TesApi.Web
         private static readonly Regex localPathRegex = GetLocalPathRegex();
         private static readonly Regex accountNameRegex = GetAccountNameRegex();
 
-        private string sasToken;
-
         /// <summary>
         /// Create from provided segments
         /// </summary>
@@ -63,11 +61,7 @@ namespace TesApi.Web
         /// <summary>
         /// The SAS token
         /// </summary>
-        public string SasToken
-        {
-            get => sasToken;
-            set => sasToken = value?.TrimStart('?');
-        }
+        public string SasToken { get; set; }
 
         /// <summary>
         /// Tries to parse the provided string. The following formats are supported:
@@ -129,7 +123,7 @@ namespace TesApi.Web
         /// </summary>
         /// <returns>Blob URL</returns>
         public string ToUriString()
-            => $"{BlobEndpoint}/{ContainerName}/{BlobName}?{SasToken}".TrimEnd('/');
+            => $"{BlobEndpoint}/{ContainerName}/{BlobName}{SasToken}".TrimEnd('/');
 
         /// <summary>
         /// Returns the Blob URI
