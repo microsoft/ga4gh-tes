e6baa01558380e7db23aaa4bb527979e99c4ae14
Matt McLoughlin <mattmcl@microsoft.com>
2023-05-03 15:00:19 -0700

    whitespace

--- Tes.Runner/Transfer/PartsReader.cs
+++ Tes.Runner/Transfer/PartsReader.cs
@@ -53,7 +53,6 @@ public class PartsReader : PartsProcessor
                             throw;
                         }
                     }
-                    
                 }
             ));
         }
