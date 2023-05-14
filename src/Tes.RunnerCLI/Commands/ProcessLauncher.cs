595f066900347e206968f1ac4fb52ce4b6fed9c5
giventocode <3589801+giventocode@users.noreply.github.com>
2023-05-14 13:08:01 -0400

    initial commit

---
+++ src/Tes.RunnerCLI/Commands/ProcessLauncher.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tes.RunnerCLI.Commands
+{
+    public class ProcessLauncher
+    {
+       private readonly string executableName = Process.GetCurrentProcess().ProcessName;
+
+       public ProcessExecutionResult LaunchProcess(string[] args)
+       {
+           var process = new Process();
+            process.StartInfo.FileName = executableName;
+            process.StartInfo.Arguments = string.Join(" ", args);
+            process.StartInfo.UseShellExecute = false;
+            process.StartInfo.RedirectStandardOutput = true;
+            process.Start();
+            process.WaitForExit();
+
+            return ToProcessExecutionResult(process);
+       }
+
+       private ProcessExecutionResult ToProcessExecutionResult(Process process)
+       {
+           return new ProcessExecutionResult(
+               process.StandardOutput.ReadToEnd(),
+               process.StandardError.ReadToEnd(),
+               process.ExitCode);
+       }
+    }
+
+    public record ProcessExecutionResult(string StandardOutput, string StandardError, int ExitCode)
+    {
+    }
+}
