93bd8a8f826a1dd160d32faf2a83b2713a55a509
Blair L Murri <BMurri@users.noreply.github.com>
2023-05-26 17:26:04 -0700

    fix path

--- src/TesApi.Web/BatchScheduler.cs
+++ src/TesApi.Web/BatchScheduler.cs
@@ -372,7 +372,7 @@ namespace TesApi.Web
         /// <inheritdoc/>
         public async Task UploadTaskRunnerIfNeeded(CancellationToken cancellationToken)
         {
-            var blobUri = new Uri(await storageAccessProvider.MapLocalPathToSasUrlAsync($"/{defaultStorageAccountName}/{TesExecutionsPathPrefix}/{NodeTaskRunnerFilename}", cancellationToken));
+            var blobUri = new Uri(await storageAccessProvider.MapLocalPathToSasUrlAsync($"/{defaultStorageAccountName}{TesExecutionsPathPrefix}/{NodeTaskRunnerFilename}", cancellationToken));
             var blobProperties = await azureProxy.GetBlobPropertiesAsync(blobUri, cancellationToken);
             if (blobProperties?.ContentMD5 != (await File.ReadAllTextAsync(Path.Combine(AppContext.BaseDirectory, $"scripts/{NodeTaskRunnerMD5HashFilename}"), cancellationToken)).Trim())
             {
