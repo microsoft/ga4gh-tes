ce5a1138b1b439172c645924143133e707efba54
Blair L Murri <BMurri@users.noreply.github.com>
2023-06-01 12:53:47 -0700

    Move changes to separate PR

--- src/Tes.RunnerCLI/Commands/CommandFactory.cs
+++ src/Tes.RunnerCLI/Commands/CommandFactory.cs
@@ -10,41 +10,33 @@ namespace Tes.RunnerCLI.Commands
     {
         const string DefaultTaskDefinitionFile = "TesTask.json";
 
-        private static readonly Uri DefaultDockerUri = new("unix:///var/run/docker.sock");
+        private static readonly Uri DefaultDockerUri = new Uri("unix:///var/run/docker.sock");
 
         internal const string UploadCommandName = "upload";
         internal const string DownloadCommandName = "download";
         internal const string DockerUriOption = "docker-url";
 
-        private static readonly IReadOnlyCollection<Option> GlobalOptions = new List<Option>()
-        {
-            CreateOption<FileInfo>(BlobPipelineOptionsConverter.FileOption, "The file with the task definition",  "-f", required: true, defaultValue: GetDefaultTaskDefinitionFile()),
-            CreateOption<int>(BlobPipelineOptionsConverter.BlockSizeOption, "Blob block size in bytes", "-b", defaultValue: BlobSizeUtils.DefaultBlockSizeBytes),
-            CreateOption<int>(BlobPipelineOptionsConverter.WritersOption, "Number of concurrent writers", "-w", defaultValue: BlobPipelineOptions.DefaultNumberOfWriters),
-            CreateOption<int>(BlobPipelineOptionsConverter.ReadersOption, "Number of concurrent readers", "-r", defaultValue: BlobPipelineOptions.DefaultNumberOfReaders),
-            CreateOption<int>(BlobPipelineOptionsConverter.BufferCapacityOption, "Pipeline buffer capacity", "-c", defaultValue: BlobPipelineOptions.DefaultReadWriteBuffersCapacity),
-            CreateOption<string>(BlobPipelineOptionsConverter.ApiVersionOption, "Azure Storage API version", "-v", defaultValue: BlobPipelineOptions.DefaultApiVersion)
-        }.AsReadOnly();
-
         internal static RootCommand CreateExecutorCommand()
         {
+            var dockerUriOption = CreateOption<Uri>(CommandFactory.DockerUriOption, "local docker engine endpoint", "-u", defaultValue: DefaultDockerUri);
+
             var rootCommand = new RootCommand("Executes the specified TES Task");
 
-            rootCommand.AddOption(CreateOption<Uri>(CommandFactory.DockerUriOption, "local docker engine endpoint", "-u", defaultValue: DefaultDockerUri));
-
-            foreach (var option in GlobalOptions)
+            foreach (var option in CreateGlobalOptionList())
             {
                 rootCommand.AddGlobalOption(option);
             }
 
+            rootCommand.AddOption(dockerUriOption);
+
             rootCommand.SetHandler(CommandHandlers.ExecuteNodeTaskAsync,
-                GetOptionByName<FileInfo>(rootCommand, BlobPipelineOptionsConverter.FileOption),
-                GetOptionByName<int>(rootCommand, BlobPipelineOptionsConverter.BlockSizeOption),
-                GetOptionByName<int>(rootCommand, BlobPipelineOptionsConverter.WritersOption),
-                GetOptionByName<int>(rootCommand, BlobPipelineOptionsConverter.ReadersOption),
-                GetOptionByName<int>(rootCommand, BlobPipelineOptionsConverter.BufferCapacityOption),
-                GetOptionByName<string>(rootCommand, BlobPipelineOptionsConverter.ApiVersionOption),
-                GetOptionByName<Uri>(rootCommand, CommandFactory.DockerUriOption));
+                GetOptionByName<FileInfo>(rootCommand.Options, BlobPipelineOptionsConverter.FileOption),
+                GetOptionByName<int>(rootCommand.Options, BlobPipelineOptionsConverter.BlockSizeOption),
+                GetOptionByName<int>(rootCommand.Options, BlobPipelineOptionsConverter.WritersOption),
+                GetOptionByName<int>(rootCommand.Options, BlobPipelineOptionsConverter.ReadersOption),
+                GetOptionByName<int>(rootCommand.Options, BlobPipelineOptionsConverter.BufferCapacityOption),
+                GetOptionByName<string>(rootCommand.Options, BlobPipelineOptionsConverter.ApiVersionOption),
+                dockerUriOption);
 
             return rootCommand;
         }
@@ -52,15 +44,16 @@ namespace Tes.RunnerCLI.Commands
         internal static Command CreateUploadCommand(RootCommand rootCommand)
         {
             var cmd = CreateCommand(UploadCommandName, "Uploads output files to blob storage");
+
             rootCommand.Add(cmd);
 
             cmd.SetHandler(CommandHandlers.ExecuteUploadTaskAsync,
-                GetOptionByName<FileInfo>(cmd, BlobPipelineOptionsConverter.FileOption),
-                GetOptionByName<int>(cmd, BlobPipelineOptionsConverter.BlockSizeOption),
-                GetOptionByName<int>(cmd, BlobPipelineOptionsConverter.WritersOption),
-                GetOptionByName<int>(cmd, BlobPipelineOptionsConverter.ReadersOption),
-                GetOptionByName<int>(cmd, BlobPipelineOptionsConverter.BufferCapacityOption),
-                GetOptionByName<string>(cmd, BlobPipelineOptionsConverter.ApiVersionOption));
+                GetOptionByName<FileInfo>(cmd.Options, BlobPipelineOptionsConverter.FileOption),
+                GetOptionByName<int>(cmd.Options, BlobPipelineOptionsConverter.BlockSizeOption),
+                GetOptionByName<int>(cmd.Options, BlobPipelineOptionsConverter.WritersOption),
+                GetOptionByName<int>(cmd.Options, BlobPipelineOptionsConverter.ReadersOption),
+                GetOptionByName<int>(cmd.Options, BlobPipelineOptionsConverter.BufferCapacityOption),
+                GetOptionByName<string>(cmd.Options, BlobPipelineOptionsConverter.ApiVersionOption));
 
             return cmd;
         }
@@ -68,15 +61,16 @@ namespace Tes.RunnerCLI.Commands
         internal static Command CreateDownloadCommand(RootCommand rootCommand)
         {
             var cmd = CreateCommand(DownloadCommandName, "Downloads input files from a HTTP source");
+
             rootCommand.Add(cmd);
 
             cmd.SetHandler(CommandHandlers.ExecuteDownloadTaskAsync,
-                GetOptionByName<FileInfo>(cmd, BlobPipelineOptionsConverter.FileOption),
-                GetOptionByName<int>(cmd, BlobPipelineOptionsConverter.BlockSizeOption),
-                GetOptionByName<int>(cmd, BlobPipelineOptionsConverter.WritersOption),
-                GetOptionByName<int>(cmd, BlobPipelineOptionsConverter.ReadersOption),
-                GetOptionByName<int>(cmd, BlobPipelineOptionsConverter.BufferCapacityOption),
-                GetOptionByName<string>(cmd, BlobPipelineOptionsConverter.ApiVersionOption));
+                GetOptionByName<FileInfo>(cmd.Options, BlobPipelineOptionsConverter.FileOption),
+                GetOptionByName<int>(cmd.Options, BlobPipelineOptionsConverter.BlockSizeOption),
+                GetOptionByName<int>(cmd.Options, BlobPipelineOptionsConverter.WritersOption),
+                GetOptionByName<int>(cmd.Options, BlobPipelineOptionsConverter.ReadersOption),
+                GetOptionByName<int>(cmd.Options, BlobPipelineOptionsConverter.BufferCapacityOption),
+                GetOptionByName<string>(cmd.Options, BlobPipelineOptionsConverter.ApiVersionOption));
 
             return cmd;
         }
@@ -85,7 +79,11 @@ namespace Tes.RunnerCLI.Commands
         {
             var cmd = new Command(optName, optDescription);
 
-            foreach (var option in options)
+            var allOptions = CreateGlobalOptionList();
+
+            allOptions.AddRange(options);
+
+            foreach (var option in allOptions)
             {
                 cmd.AddOption(option);
             }
@@ -93,47 +91,41 @@ namespace Tes.RunnerCLI.Commands
             return cmd;
         }
 
+        private static List<Option> CreateGlobalOptionList()
+        {
+            return new List<Option>()
+            {
+                CreateOption<FileInfo>(BlobPipelineOptionsConverter.FileOption, "The file with the task definition",  "-f", required: true, defaultValue: GetDefaultTaskDefinitionFile()),
+                CreateOption<int>(BlobPipelineOptionsConverter.BlockSizeOption, "Blob block size in bytes", "-b", defaultValue: BlobSizeUtils.DefaultBlockSizeBytes),
+                CreateOption<int>(BlobPipelineOptionsConverter.WritersOption, "Number of concurrent writers", "-w", defaultValue: BlobPipelineOptions.DefaultNumberOfWriters),
+                CreateOption<int>(BlobPipelineOptionsConverter.ReadersOption, "Number of concurrent readers", "-r", defaultValue: BlobPipelineOptions.DefaultNumberOfReaders),
+                CreateOption<int>(BlobPipelineOptionsConverter.BufferCapacityOption, "Pipeline buffer capacity", "-c", defaultValue: BlobPipelineOptions.DefaultReadWriteBuffersCapacity),
+                CreateOption<string>(BlobPipelineOptionsConverter.ApiVersionOption, "Azure Storage API version", "-v", defaultValue: BlobPipelineOptions.DefaultApiVersion),
+            };
+        }
+
         private static FileInfo GetDefaultTaskDefinitionFile()
         {
             return new FileInfo(DefaultTaskDefinitionFile);
         }
 
-        private static Option<T> GetOptionByName<T>(Command command, string optionName)
+        private static Option<T> GetOptionByName<T>(IReadOnlyCollection<Option> commandOptions, string optionName)
         {
-            var options = Enumerable.Empty<Option>().Concat(command.Options);
+            var option = commandOptions.SingleOrDefault(o => o.Name == optionName);
 
-            var option = options.SingleOrDefault(o => o.Name == optionName) as Option<T>;
-
-            if (option is not null)
+            if (option != null)
             {
-                return option;
+                return (Option<T>)option;
             }
 
-            foreach (var cmd in command.Parents.OfType<Command>())
-            {
-                option = cmd.Options.Where(IsOptionGlobal).SingleOrDefault(o => o.Name == optionName) as Option<T>;
-
-                if (option is not null)
-                {
-                    return option;
-                }
-            }
-
-            throw new InvalidOperationException("Invalid option (name not found)");
-
-            // TODO: Future versions of System.CommandLine are expected to make the flag public, but it appears that it will also be renamed. So we can then get rid of the reflection.
-            static bool IsOptionGlobal(Option option)
-                => (bool)option.GetType().GetProperty("IsGlobal", System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.NonPublic)!.GetValue(option)!;
+            throw new InvalidOperationException("Invalid option");
         }
 
-        private static Option<T> CreateOption<T>(string name, string description, string alias, bool required = false, object? defaultValue = null, System.CommandLine.Parsing.ParseArgument<T>? parse = null, Action<Option<T>>? configureOption = null)
+        private static Option<T> CreateOption<T>(string name, string description, string alias, bool required = false, object? defaultValue = null)
         {
-            var option = parse is null
-                ? new Option<T>(name: $"--{name}",
-                               description: description)
-                : new Option<T>(name: $"--{name}",
-                               description: description,
-                               parseArgument: parse);
+            var option = new Option<T>(
+                                name: $"--{name}",
+                               description: description);
 
             if (defaultValue != null)
             {
@@ -142,8 +134,6 @@ namespace Tes.RunnerCLI.Commands
 
             option.AddAlias(alias);
             option.IsRequired = required;
-
-            configureOption?.Invoke(option!);
 
             return option;
         }
