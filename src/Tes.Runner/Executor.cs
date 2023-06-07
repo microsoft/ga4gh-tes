37d2e23795d27f3b0ca9178d93fa712faa767089
giventocode <3589801+giventocode@users.noreply.github.com>
2023-06-07 18:34:28 -0400

    fixed formatting

--- src/Tes.Runner/Executor.cs
+++ src/Tes.Runner/Executor.cs
@@ -156,7 +156,7 @@ namespace Tes.Runner
         {
             if (blockSizeBytes % BlobSizeUtils.BlockSizeIncrementUnitInBytes > 0)
             {
-                throw new InvalidOperationException($"The provided block size: {blockSizeBytes:n:0} is not valid for the upload operation. The block size must be a multiple of {BlobSizeUtils.BlockSizeIncrementUnitInBytes / BlobSizeUtils.MiB} MiB ({BlobSizeUtils.BlockSizeIncrementUnitInBytes:n:0} bytes)");
+                throw new InvalidOperationException($"The provided block size: {blockSizeBytes:n0} is not valid for the upload operation. The block size must be a multiple of {BlobSizeUtils.BlockSizeIncrementUnitInBytes / BlobSizeUtils.MiB} MiB ({BlobSizeUtils.BlockSizeIncrementUnitInBytes:n0} bytes)");
             }
         }
 
