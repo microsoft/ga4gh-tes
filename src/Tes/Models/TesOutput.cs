e042bb9caefb5fc0ff6cd0a6b42aa321ac751f73
giventocode <3589801+giventocode@users.noreply.github.com>
2023-06-13 19:36:52 -0400

    path prefix

--- src/Tes/Models/TesOutput.cs
+++ src/Tes/Models/TesOutput.cs
@@ -61,6 +61,12 @@ namespace Tes.Models
         public TesFileType Type { get; set; }
 
         /// <summary>
+        /// Gets or Sets Path Prefix
+        /// </summary>
+        [DataMember(Name = "pathPrefix")]
+        public string PathPrefix { get; set; }
+
+        /// <summary>
         /// Returns the string presentation of the object
         /// </summary>
         /// <returns>String presentation of the object</returns>
@@ -72,6 +78,7 @@ namespace Tes.Models
                 .Append("  Url: ").Append(Url).Append('\n')
                 .Append("  Path: ").Append(Path).Append('\n')
                 .Append("  Type: ").Append(Type).Append('\n')
+                .Append("  PathPrefix: ").Append(PathPrefix).Append('\n')
                 .Append("}\n")
                 .ToString();
 
@@ -129,7 +136,11 @@ namespace Tes.Models
                 (
                     Type == other.Type ||
                     Type.Equals(other.Type)
-                ),
+                ) &&
+                (
+                    PathPrefix == other.PathPrefix || 
+                    PathPrefix.Equals(other.PathPrefix)
+                    ),
             };
 
         /// <summary>
@@ -160,6 +171,11 @@ namespace Tes.Models
                 if (Path is not null)
                 {
                     hashCode = hashCode * 59 + Path.GetHashCode();
+                }
+
+                if (PathPrefix is not null)
+                {
+                    hashCode = hashCode * 59 + PathPrefix.GetHashCode();
                 }
 
                 hashCode = hashCode * 59 + Type.GetHashCode();
