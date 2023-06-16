fbbcaf14f4a3d976925e62d8a3c284a28b93ca28
Jonathon Saunders <jsaun@users.noreply.github.com>
2023-06-16 12:09:20 -0700

    Use AAD integration for admin group (#263)
    * Use aad integration
    
    * Make aad profile optional

--- src/deploy-tes-on-azure/Deployer.cs
+++ src/deploy-tes-on-azure/Deployer.cs
@@ -915,6 +915,18 @@ namespace TesDeployer
                     UserAssignedIdentities = new Dictionary<string, ManagedClusterIdentityUserAssignedIdentitiesValue>()
                 }
             };
+
+            if (!string.IsNullOrWhiteSpace(configuration.AadGroupIds))
+            {
+                cluster.EnableRBAC = true;
+                cluster.AadProfile = new ManagedClusterAADProfile()
+                {
+                    AdminGroupObjectIDs = configuration.AadGroupIds.Split(",", StringSplitOptions.RemoveEmptyEntries),
+                    EnableAzureRBAC = false,
+                    Managed = true
+                };
+            }
+
             cluster.Identity.UserAssignedIdentities.Add(managedIdentity.Id, new(managedIdentity.PrincipalId, managedIdentity.ClientId));
             cluster.IdentityProfile = new Dictionary<string, ManagedClusterPropertiesIdentityProfileValue>
             {
