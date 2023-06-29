b8bec6ea1471b33d7099aad32a457c91a126d9d5
Blair L Murri <BMurri@users.noreply.github.com>
2023-06-28 17:08:33 -0700

    Consider a missing job as equivalent to a missing task in that job (#271)
    when deleting the task

--- src/TesApi.Web/AzureProxy.cs
+++ src/TesApi.Web/AzureProxy.cs
@@ -502,7 +502,17 @@ namespace TesApi.Web
                 SelectClause = "id"
             };
 
-            var batchTasksToDelete = await batchClient.JobOperations.ListTasks(pool.PoolId, jobFilter).ToAsyncEnumerable().ToListAsync(cancellationToken);
+            List<CloudTask> batchTasksToDelete = default;
+
+            try
+            {
+                batchTasksToDelete = await batchClient.JobOperations.ListTasks(pool.PoolId, jobFilter).ToAsyncEnumerable().ToListAsync(cancellationToken);
+            }
+            catch (BatchException ex) when (ex.InnerException is Microsoft.Azure.Batch.Protocol.Models.BatchErrorException bee && "JobNotFound".Equals(bee.Body?.Code, StringComparison.InvariantCultureIgnoreCase))
+            {
+                logger.LogWarning("Job not found for TES task {TesTask}", tesTaskId);
+                return; // Task cannot exist if the job is not found.
+            }
 
             if (batchTasksToDelete.Count > 1)
             {
