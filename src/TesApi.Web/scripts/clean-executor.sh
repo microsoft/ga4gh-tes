2dd339fe673153daf7c712de835b7311ae137846
Matt McLoughlin <mattmcl@microsoft.com>
2023-07-20 09:53:20 -0700

    Prunes all images (not just dangling ones)

--- src/TesApi.Web/scripts/clean-executor.sh
+++ src/TesApi.Web/scripts/clean-executor.sh
@@ -2,4 +2,4 @@ cd "$(dirname "$0")"
 if [ "$1" != "{TaskExecutor}" ]; then docker rmi {TaskExecutor} -f; fi || :
 rm -fdr wd/{ExecutionPathPrefix} || :
 sudo docker container prune -f || :
-sudo docker system prune --volumes -f || :
+sudo docker system prune -a --volumes -f || :
