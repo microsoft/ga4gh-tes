b0f88ce54cc39959c747d93095211e8623497549
giventocode <3589801+giventocode@users.noreply.github.com>
2023-08-28 17:35:07 -0400

    initial changes

---
+++ src/Tes.Runner/Docker/VolumeBindingsGenerator.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+namespace Tes.Runner.Docker
+{
+    public class VolumeBindingsGenerator
+    {
+        private readonly string mountParentDirectory;
+
+        public VolumeBindingsGenerator(string mountParentDirectory)
+        {
+            ArgumentException.ThrowIfNullOrEmpty(mountParentDirectory, nameof(mountParentDirectory));
+
+            this.mountParentDirectory = mountParentDirectory;
+        }
+    }
+}
