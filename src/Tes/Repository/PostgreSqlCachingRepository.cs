e1c340e717398a80f55a66d9a0454ee333d81ccc
Matt McLoughlin <mattmcl@microsoft.com>
2023-09-18 09:42:01 -0700

    remove commented code

--- src/Tes/Repository/PostgreSqlCachingRepository.cs
+++ src/Tes/Repository/PostgreSqlCachingRepository.cs
@@ -213,22 +213,12 @@ namespace Tes.Repository
                     _writerWorkerTask.Wait();
                 }
 
-                // TODO: free unmanaged resources (unmanaged objects) and override finalizer
-                // TODO: set large fields to null
                 _disposedValue = true;
             }
         }
 
-        // // TODO: override finalizer only if 'Dispose(bool disposing)' has code to free unmanaged resources
-        // ~PostgreSqlCachingRepository()
-        // {
-        //     // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
-        //     Dispose(disposing: false);
-        // }
-
         public void Dispose()
         {
-            // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
             Dispose(disposing: true);
             GC.SuppressFinalize(this);
         }
