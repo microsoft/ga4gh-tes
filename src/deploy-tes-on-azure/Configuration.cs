e63db911c6a1f1f0bfc2448f2a4c41c7a871000f
Jonathon Saunders <jsaun@users.noreply.github.com>
2023-09-21 08:46:44 -0700

    Reduce aks node pool size to 1

--- src/deploy-tes-on-azure/Configuration.cs
+++ src/deploy-tes-on-azure/Configuration.cs
@@ -52,7 +52,7 @@ namespace TesDeployer
         public bool ManualHelmDeployment { get; set; }
         public string HelmBinaryPath { get; set; } = OperatingSystem.IsWindows() ? @"C:\ProgramData\chocolatey\bin\helm.exe" : "/usr/local/bin/helm";
         public string KubectlBinaryPath { get; set; } = OperatingSystem.IsWindows() ? @"C:\ProgramData\chocolatey\bin\kubectl.exe" : "/usr/local/bin/kubectl";
-        public int AksPoolSize { get; set; } = 2;
+        public int AksPoolSize { get; set; } = 1;
         public bool? CrossSubscriptionAKSDeployment { get; set; } = null;
         public bool Silent { get; set; }
         public bool DeleteResourceGroupOnFailure { get; set; }
