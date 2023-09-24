35900c283eb067dca0fde4bda9b204d74169de4e
giventocode <3589801+giventocode@users.noreply.github.com>
2023-09-23 22:37:24 -0400

    refactoring and additional tests

--- src/TesApi.Tests/BatchNodeScriptBuilderTests.cs
+++ src/TesApi.Tests/BatchNodeScriptBuilderTests.cs
@@ -1,11 +1,9 @@
 ﻿// Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
+using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using TesApi.Web;
-
-// Copyright (c) Microsoft Corporation.
-// Licensed under the MIT License.
 
 namespace TesApi.Tests
 {
@@ -20,7 +18,52 @@ namespace TesApi.Tests
             builder = new BatchNodeScriptBuilder();
         }
 
-        [TestMethod()]
+        [TestMethod]
+        public void CreateWgetDownloadCommand_ValidUrl_ReturnsCommand()
+        {
+            var url = "https://foo.bar";
+            var local = "/local";
+            var expectedCommand = $"wget --https-only --timeout=20 --waitretry=1 --tries=9 --retry-connrefused --continue -O {local} '{url}'";
+
+            var result = BatchNodeScriptBuilder.CreateWgetDownloadCommand(url, "/local");
+
+            Assert.AreEqual(expectedCommand, result);
+        }
+
+        [TestMethod]
+        public void CreateWgetDownloadCommand_InvalidUrl_ThrowsArgumentException()
+        {
+            var url = "foo.bar";
+            var local = "/local";
+            Assert.ThrowsException<ArgumentException>(() => BatchNodeScriptBuilder.CreateWgetDownloadCommand(url, local));
+        }
+
+
+        [TestMethod]
+        public void WithAlpineWgetInstallation_BuildCalled_ScriptContainsWgetInstallation()
+        {
+            var expectedLine = @"(grep -q alpine /etc/os-release && apk add bash wget || :) && \";
+
+            var result = builder.WithAlpineWgetInstallation()
+                .Build();
+            Assert.IsTrue(result.Contains(expectedLine));
+        }
+
+        [TestMethod]
+        public void WithRunnerFilesDownloadUsingWget_BuildCalled_WgetCallsAreCreated()
+        {
+            var expectedLine1 = BatchNodeScriptBuilder.CreateWgetDownloadCommand("https://foo.bar1", $"${BatchNodeScriptBuilder.BatchTaskDirEnvVarName}/{BatchNodeScriptBuilder.NodeTaskRunnerFilename}", setExecutable: true);
+            var expectedLine2 = BatchNodeScriptBuilder.CreateWgetDownloadCommand("https://foo.bar2", $"${BatchNodeScriptBuilder.BatchTaskDirEnvVarName}/{BatchNodeScriptBuilder.NodeRunnerTaskDefinitionFilename}", setExecutable: false);
+
+            var result = builder
+                .WithRunnerFilesDownloadUsingWget("https://foo.bar1", "https://foo.bar2")
+                .Build();
+
+            Assert.IsTrue(result.Contains(expectedLine1));
+            Assert.IsTrue(result.Contains(expectedLine2));
+        }
+
+        [TestMethod]
         public void Build_ScriptEndsWithEchoTaskComplete()
         {
             var result = builder.WithMetrics()
---
+++ src/TesApi.Tests/Runner/TaskToNodeTaskConverterTests.cs
@@ -0,0 +1,175 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using System;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging.Abstractions;
+using Microsoft.Extensions.Options;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Newtonsoft.Json;
+using Tes.Models;
+using TesApi.Web.Management.Configuration;
+using TesApi.Web.Runner;
+using TesApi.Web.Storage;
+
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+namespace TesApi.Tests.Runner
+{
+    [TestClass, TestCategory("Unit")]
+    public class TaskToNodeTaskConverterTests
+    {
+        private Mock<IStorageAccessProvider> storageAccessProviderMock;
+        private TaskToNodeTaskConverter taskToNodeTaskConverter;
+        private readonly TesTask tesTask = GetTestTesTask();
+        private TerraOptions terraOptions;
+        const string InternalBlobUrl = "http://foo.bar/tes-internal";
+        const string InternalBlobUrlWithSas = $"{InternalBlobUrl}?qs=sig";
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            terraOptions = new TerraOptions();
+            storageAccessProviderMock = new Mock<IStorageAccessProvider>();
+            storageAccessProviderMock.Setup(x =>
+                                   x.GetInternalTesTaskBlobUrlAsync(It.IsAny<TesTask>(), It.IsAny<string>(), It.IsAny<CancellationToken>()))
+                .ReturnsAsync(InternalBlobUrlWithSas);
+
+            taskToNodeTaskConverter = new TaskToNodeTaskConverter(Options.Create(terraOptions), storageAccessProviderMock.Object, new NullLogger<TaskToNodeTaskConverter>());
+        }
+        [TestMethod]
+        public async Task ToNodeTaskAsync_TesTaskWithContentInputs_ContentInputsAreUploadedAndSetInTaskDefinition()
+        {
+            var contentInput = tesTask.Inputs.Find(i => !string.IsNullOrWhiteSpace(i.Content));
+            var nodeTask = await taskToNodeTaskConverter.ToNodeTaskAsync(tesTask, additionalInputs: null, new RuntimeContainerCleanupOptions(false, false), CancellationToken.None);
+
+            Assert.IsNotNull(nodeTask);
+
+            // Verify that the content input was uploaded and the node task has the correct url
+            storageAccessProviderMock.Verify(x => x.UploadBlobAsync(It.IsAny<Uri>(), It.IsAny<string>(), It.IsAny<CancellationToken>()), Times.Exactly(1));
+            // The input must be uploaded to the internal blob url without a sas token.
+            Assert.AreEqual(InternalBlobUrl, nodeTask.Inputs!.Find(i => i.Path == $"{TaskToNodeTaskConverter.BatchTaskWorkingDirEnvVar}{contentInput!.Path}")!.SourceUrl);
+        }
+
+        [TestMethod]
+        public async Task
+            ToNodeTaskAsync_TesTaskWithInputsAndOutputs_AllInputsAndOutputsArePrefixedWithBatchTaskWorkingDir()
+        {
+            var nodeTask = await taskToNodeTaskConverter.ToNodeTaskAsync(tesTask, additionalInputs: null, new RuntimeContainerCleanupOptions(false, false), CancellationToken.None);
+            Assert.IsNotNull(nodeTask);
+            // Verify that all inputs and outputs are prefixed with the batch task working dir
+            foreach (var input in nodeTask.Inputs!)
+            {
+                Assert.IsTrue(input.Path!.StartsWith(TaskToNodeTaskConverter.BatchTaskWorkingDirEnvVar));
+            }
+
+            foreach (var output in nodeTask.Outputs!)
+            {
+                Assert.IsTrue(output.Path!.StartsWith(TaskToNodeTaskConverter.BatchTaskWorkingDirEnvVar));
+            }
+        }
+
+        [TestMethod]
+        public async Task ToNodeTaskAsync_AdditionalInputsProvided_NodeTesTaskContainsTheAdditionalInputs()
+        {
+            var additionalInputs = CreateAdditionalInputs();
+
+            var nodeTask = await taskToNodeTaskConverter.ToNodeTaskAsync(tesTask, additionalInputs, new RuntimeContainerCleanupOptions(false, false), CancellationToken.None);
+
+            Assert.IsNotNull(nodeTask);
+
+            //verify the additional inputs are added to the node task
+            var expectedPath = $"{TaskToNodeTaskConverter.BatchTaskWorkingDirEnvVar}{additionalInputs[0].Path}";
+            Assert.IsTrue(nodeTask.Inputs!.Any(i => i.Path!.Equals(expectedPath, StringComparison.OrdinalIgnoreCase)));
+            expectedPath = $"{TaskToNodeTaskConverter.BatchTaskWorkingDirEnvVar}{additionalInputs[1].Path}";
+            Assert.IsTrue(nodeTask.Inputs!.Any(i => i.Path!.Equals(expectedPath, StringComparison.OrdinalIgnoreCase)));
+        }
+
+
+        [TestMethod]
+        public async Task ToNodeTaskAsync_TesTaskWithNoInputsAndOutputs_NodeTaskContainsNoInputsAndOutputs()
+        {
+            var task = GetTestTesTask();
+            task.Inputs = null;
+            task.Outputs = null;
+            var nodeTask = await taskToNodeTaskConverter.ToNodeTaskAsync(task, additionalInputs: null, new RuntimeContainerCleanupOptions(false, false), CancellationToken.None);
+            Assert.IsNotNull(nodeTask);
+            Assert.IsNull(nodeTask.Inputs);
+            Assert.IsNull(nodeTask.Outputs);
+        }
+
+        [TestMethod]
+        public async Task
+            ToNodeTaskAsync_TesTaskWithNoInputsAndOutputsAndAdditionalInputs_NodeTaskContainsOnlyAdditionalInputs()
+        {
+            var task = GetTestTesTask();
+            task.Inputs = null;
+            task.Outputs = null;
+            var additionalInputs = CreateAdditionalInputs();
+            var nodeTask = await taskToNodeTaskConverter.ToNodeTaskAsync(task, additionalInputs, new RuntimeContainerCleanupOptions(false, false), CancellationToken.None);
+
+            Assert.IsNotNull(nodeTask);
+            Assert.AreEqual(2, nodeTask.Inputs!.Count);
+            Assert.IsNull(nodeTask.Outputs);
+        }
+
+        [TestMethod]
+        public async Task
+            ToNodeTaskAsync_TesTaskWithNoInputsAndOutputsAndNoAdditionalInputs_NodeTaskContainsNoInputsAndOutputs()
+        {
+            var task = GetTestTesTask();
+            task.Inputs = null;
+            task.Outputs = null;
+            var nodeTask = await taskToNodeTaskConverter.ToNodeTaskAsync(task, additionalInputs: null, new RuntimeContainerCleanupOptions(false, false), CancellationToken.None);
+            Assert.IsNotNull(nodeTask);
+            Assert.IsNull(nodeTask.Inputs);
+            Assert.IsNull(nodeTask.Outputs);
+        }
+
+        [TestMethod]
+        public async Task ToNodeTaskAsync_AdditionalInputsContainsAnExistingInput_OnlyDistinctAdditionalInputsAreAdded()
+        {
+            var additionalInputs = CreateAdditionalInputs();
+            additionalInputs[0].Path = tesTask.Inputs.First().Path;
+
+            var nodeTask = await taskToNodeTaskConverter.ToNodeTaskAsync(tesTask, additionalInputs, new RuntimeContainerCleanupOptions(false, false), CancellationToken.None);
+            Assert.IsNotNull(nodeTask);
+
+            //verify that only one additional inputs is added to the node task
+            Assert.AreEqual(tesTask.Inputs.Count + 1, nodeTask.Inputs!.Count);
+            var expectedPath = $"{TaskToNodeTaskConverter.BatchTaskWorkingDirEnvVar}{additionalInputs[1].Path}";
+            Assert.IsTrue(nodeTask.Inputs!.Any(i => i.Path!.Equals(expectedPath, StringComparison.OrdinalIgnoreCase)));
+        }
+
+        private static TesInput[] CreateAdditionalInputs()
+        {
+            return new[]
+            {
+                new TesInput
+                {
+                    Name = "additionalInput1",
+                    Path = "/additionalInput1",
+                    Type = TesFileType.FILEEnum,
+                    Url = "http://foo.bar/additionalInput1"
+                },
+                new TesInput
+                {
+                    Name = "additionalInput2",
+                    Path = "/additionalInput2",
+                    Type = TesFileType.FILEEnum,
+                    Url = "http://foo.bar/additionalInput2"
+                }
+            };
+        }
+
+        private static TesTask GetTestTesTask()
+        {
+            return JsonConvert.DeserializeObject<TesTask>(File.ReadAllText("testask1.json"));
+        }
+    }
+}
