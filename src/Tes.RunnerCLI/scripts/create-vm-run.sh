bf7180156bade3d642c372c6d2bdc2f143176a97
Matt McLoughlin <mattmcl@microsoft.com>
2023-12-06 16:09:05 -0800

    update script

--- src/Tes.RunnerCLI/scripts/create-vm-run.sh
+++ src/Tes.RunnerCLI/scripts/create-vm-run.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
+# To run from WSL (where 'mattmcl' is your username):
 # chmod +x /mnt/c/Users/mattmcl/Source/Repos/microsoft/ga4gh-tes/src/Tes.RunnerCLI/scripts/create-vm-run.sh
 # /mnt/c/Users/mattmcl/Source/Repos/microsoft/ga4gh-tes/src/Tes.RunnerCLI/scripts/create-vm-run.sh
 
-# Script to create an Azure VM with randomly generated names for resource group and VM,
-# set a resource tag, and handle script cancellation
-
-# Check for required command line arguments
 if [ "$#" -ne 3 ]; then
     echo "Usage: $0 SUBSCRIPTION_ID REGION OWNER_TAG_VALUE"
     exit 1
@@ -16,17 +13,14 @@ SUBSCRIPTION_ID=$1
 REGION=$2
 OWNER_TAG_VALUE=$3
 
-# Function to generate a random lowercase string of letters
 generate_random_name() {
     cat /dev/urandom | tr -dc 'a-z' | fold -w ${1:-10} | head -n 1
 }
 
-# Generate random names for resource group and VM
 RANDOM_NAME=$(generate_random_name)
 RESOURCE_GROUP_NAME="${OWNER_TAG_VALUE}-${RANDOM_NAME}"
 VM_NAME=$(generate_random_name)
 
-# Function to handle script interruption
 cleanup() {
     echo
     read -p "Do you want to delete the resource group? [Y/n]: " -r -e -i "Y" response
@@ -44,13 +38,13 @@ cleanup() {
 
 # Trap SIGINT (Ctrl+C) and call the cleanup function
 trap cleanup SIGINT
-
+az login
 echo "Setting subscription ID..."
-# Set the provided subscription ID
+
 az account set --subscription $SUBSCRIPTION_ID
 
 echo "Creating resource group: $RESOURCE_GROUP_NAME in region: $REGION with owner tag: $OWNER_TAG_VALUE..."
-# Create a resource group with the random name in the provided region and set the OWNER tag
+
 az group create --name $RESOURCE_GROUP_NAME --location $REGION --tags OWNER=$OWNER_TAG_VALUE
 
 echo "Creating virtual machine: $VM_NAME with Ubuntu 22.04..."
@@ -78,13 +72,11 @@ az vm open-port --port 22 --resource-group $RESOURCE_GROUP_NAME --name $VM_NAME
 echo "Uploading and running 'run.sh' script on the VM..."
 # Upload and run 'run.sh' script from the current directory
 az vm extension set \
-    --resource-group $RESOURCE_GROUP_NAME \
-    --vm-name $VM_NAME \
+    --resource-group exttest \
+    --vm-name exttest \
     --name customScript \
     --publisher Microsoft.Azure.Extensions \
-    --version 2.0 \
-    --settings '{"script": "cat ./clone-build-run.sh | bash -s > /tmp/run_output.log"}'
-
+    --protected-settings '{"fileUris": ["https://raw.githubusercontent.com/microsoft/ga4gh-tes/5df9d9b478cb27ff2ac0c84e7e7a7c83a0b85ebd/src/Tes.RunnerCLI/scripts/clone-build-run.sh"],"commandToExecute": "./clone-build-run.sh"}'
 
 # Continuously check if the script execution has completed
 while true; do
