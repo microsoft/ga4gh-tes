4f298bc47f6bfb43d10b4647e2041bc05034d42b
Matt McLoughlin <mattmcl@microsoft.com>
2023-12-06 21:53:17 -0800

    script

--- src/Tes.RunnerCLI/scripts/create-vm-run.sh
+++ src/Tes.RunnerCLI/scripts/create-vm-run.sh
@@ -64,12 +64,13 @@ STORAGE_ACCOUNT_KEY=$(az storage account keys list --resource-group $RESOURCE_GR
 az storage container create --name tes-internal --account-name $STORAGE_ACCOUNT_NAME --account-key $STORAGE_ACCOUNT_KEY
 
 print_green "Creating virtual machine: $VM_NAME with Ubuntu 22.04..."
-az vm create \
+VM_PUBLIC_IP=$(az vm create \
     --resource-group $RESOURCE_GROUP_NAME \
     --name $VM_NAME \
     --image Ubuntu2204 \
     --admin-username azureuser \
-    --generate-ssh-keys
+    --generate-ssh-keys \
+    --query publicIpAddress -o tsv)
 
 IDENTITY_ID=$(az identity show --name $IDENTITY_NAME --resource-group $RESOURCE_GROUP_NAME --query id -o tsv)
 print_green "Assigning the identity $IDENTITY_NAME to $VM_NAME..."
@@ -78,7 +79,7 @@ az vm identity assign -g $RESOURCE_GROUP_NAME -n $VM_NAME --identities $IDENTITY
 print_green "Opening port 22 for SSH access..."
 az vm open-port --port 22 --resource-group $RESOURCE_GROUP_NAME --name $VM_NAME
 
-print_green "Uploading and running 'clone-build-run.sh' script on the VM..."
+print_green "Running 'clone-build-run.sh' script on the VM..."
 az vm extension set \
     --resource-group $RESOURCE_GROUP_NAME \
     --vm-name $VM_NAME \
@@ -91,30 +92,25 @@ elapsed_seconds=0
 while true; do
     print_green "Checking if script done..."
 
-    script_status=$(az vm extension show --resource-group $RESOURCE_GROUP_NAME --vm-name $VM_NAME --name customScript --query 'instanceView.status' -o tsv)
+    script_status=$(az vm extension show --resource-group $RESOURCE_GROUP_NAME --vm-name $VM_NAME --name customScript --query 'provisioningState' -o tsv)
     
     if [ "$script_status" == "Succeeded" ]; then
-        print_green "Script succeeded."
+        print_green "Extension applied successfully. Checking script output for completion status..."
         break
     elif [ "$script_status" == "Failed" ]; then
-        print_green "Script execution on the VM has failed."
+        print_green "Extension application failed."
         exit 1
     fi
 
-    print_green "Script status: $script_status"
+    print_green "Current provisioning state of the extension: $script_status"
     print_green "Elapsed time: $elapsed_seconds seconds."
     sleep 10
     elapsed_seconds=$((elapsed_seconds + 10))
 done
 
+
 print_green "Downloading script output..."
-az vm extension file copy \
-    --resource-group $RESOURCE_GROUP_NAME \
-    --vm-name $VM_NAME \
-    --publisher Microsoft.Azure.Extensions \
-    --extension-name customScript \
-    --name /tmp/run_output.log \
-    --destination ./run_output.log
+scp azureuser@$VM_PUBLIC_IP:/tmp/run_output.log ./run_output.log
 
 print_green "Script completed. Resource group remains active."
 print_green "Resource Group: $RESOURCE_GROUP_NAME"
