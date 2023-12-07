62d81c8a2a3c19a6efe5b957770aa11f774070bf
Jesus Aguilar <3589801+giventocode@users.noreply.github.com>
2023-12-07 00:27:00 -0500

    handles fatal errors (#536)

---
+++ src/Tes.Runner.Test/Commands/CommandLauncherTests.cs
@@ -0,0 +1,37 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using Moq;
+using Tes.Runner.Exceptions;
+using Tes.RunnerCLI.Commands;
+
+namespace Tes.Runner.Test.Commands
+{
+    [TestClass]
+    [TestCategory("Unit")]
+    public class CommandLauncherTests
+    {
+
+        [TestMethod]
+        public void HandleFatalLauncherError_IdentityUnavailableException_ReturnsIdentityUnavailableExitCode()
+        {
+            var exception = new IdentityUnavailableException();
+            var expectedExitCode = (int)ProcessExitCode.IdentityUnavailable;
+
+            var ex = Assert.ThrowsException<CommandExecutionException>(() => CommandLauncher.HandleFatalLauncherError("command", exception));
+
+            Assert.AreEqual(expectedExitCode, ex.ExitCode);
+        }
+
+        [TestMethod]
+        public void HandleFatalLauncherError_Exception_ReturnsUncategorizedExitCode()
+        {
+            var exception = new Exception();
+            var expectedExitCode = (int)ProcessExitCode.UncategorizedError;
+
+            var ex = Assert.ThrowsException<CommandExecutionException>(() => CommandLauncher.HandleFatalLauncherError("command", exception));
+
+            Assert.AreEqual(expectedExitCode, ex.ExitCode);
+        }
+    }
+}
