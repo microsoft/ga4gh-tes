d38c569643b6965372981322c348987b15493840
Blair L Murri <BMurri@users.noreply.github.com>
2024-01-25 16:46:56 -0800

    Remove serialization support from Exception-derived classes

--- src/TesApi.Web/Exceptions/AzureBatchLowQuotaException.cs
+++ src/TesApi.Web/Exceptions/AzureBatchLowQuotaException.cs
@@ -2,11 +2,9 @@
 // Licensed under the MIT License.
 
 using System;
-using System.Runtime.Serialization;
 
 namespace TesApi.Web
 {
-    [Serializable]
     internal class AzureBatchLowQuotaException : Exception
     {
         public AzureBatchLowQuotaException()
@@ -18,10 +16,6 @@ namespace TesApi.Web
         }
 
         public AzureBatchLowQuotaException(string message, Exception innerException) : base(message, innerException)
-        {
-        }
-
-        protected AzureBatchLowQuotaException(SerializationInfo info, StreamingContext context) : base(info, context)
         {
         }
     }
