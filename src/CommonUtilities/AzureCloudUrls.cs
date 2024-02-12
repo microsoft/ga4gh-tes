b2f21c9e92b056a676b7e8b8f3382b2aee20ded1
Matt McLoughlin <mattmcl@microsoft.com>
2024-02-12 11:25:05 -0800

    add retry policy

--- src/CommonUtilities/AzureCloudUrls.cs
+++ src/CommonUtilities/AzureCloudUrls.cs
@@ -3,6 +3,7 @@
 
 using System.Text.Json;
 using System.Text.Json.Serialization;
+using Polly;
 
 namespace CommonUtilities.AzureCloud
 {
@@ -11,75 +12,78 @@ namespace CommonUtilities.AzureCloud
         private const string defaultAzureCloudMetadataUrl = "https://management.azure.com/metadata/endpoints?api-version=2023-11-01";
 
         [JsonPropertyName("portal")]
-        public string PortalUrl { get; set; }
+        public string? PortalUrl { get; set; }
 
         [JsonPropertyName("authentication")]
-        public AuthenticationDetails Authentication { get; set; }
+        public AuthenticationDetails? Authentication { get; set; }
 
         [JsonPropertyName("media")]
-        public string MediaUrl { get; set; }
+        public string? MediaUrl { get; set; }
 
         [JsonPropertyName("graphAudience")]
-        public string GraphAudienceUrl { get; set; }
+        public string? GraphAudienceUrl { get; set; }
 
         [JsonPropertyName("graph")]
-        public string GraphUrl { get; set; }
+        public string? GraphUrl { get; set; }
 
         [JsonPropertyName("name")]
-        public string Name { get; set; }
+        public string? Name { get; set; }
 
         [JsonPropertyName("suffixes")]
-        public EndpointSuffixes Suffixes { get; set; }
+        public EndpointSuffixes? Suffixes { get; set; }
 
         [JsonPropertyName("batch")]
-        public string BatchUrl { get; set; }
+        public string? BatchUrl { get; set; }
 
         [JsonPropertyName("resourceManager")]
-        public string ResourceManagerUrl { get; set; }
+        public string? ResourceManagerUrl { get; set; }
 
         [JsonPropertyName("vmImageAliasDoc")]
-        public string VmImageAliasDocumentationUrl { get; set; }
+        public string? VmImageAliasDocumentationUrl { get; set; }
 
         [JsonPropertyName("sqlManagement")]
-        public string SqlManagementUrl { get; set; }
+        public string? SqlManagementUrl { get; set; }
 
         [JsonPropertyName("microsoftGraphResourceId")]
-        public string MicrosoftGraphResourceUrl { get; set; }
+        public string? MicrosoftGraphResourceUrl { get; set; }
 
         [JsonPropertyName("appInsightsResourceId")]
-        public string ApplicationInsightsResourceUrl { get; set; }
+        public string? ApplicationInsightsResourceUrl { get; set; }
 
         [JsonPropertyName("appInsightsTelemetryChannelResourceId")]
-        public string ApplicationInsightsTelemetryChannelResourceUrl { get; set; }
+        public string? ApplicationInsightsTelemetryChannelResourceUrl { get; set; }
 
         [JsonPropertyName("synapseAnalyticsResourceId")]
-        public string SynapseAnalyticsResourceUrl { get; set; }
+        public string? SynapseAnalyticsResourceUrl { get; set; }
 
         [JsonPropertyName("logAnalyticsResourceId")]
-        public string LogAnalyticsResourceUrl { get; set; }
+        public string? LogAnalyticsResourceUrl { get; set; }
 
         [JsonPropertyName("ossrDbmsResourceId")]
-        public string OssrDbmsResourceUrl { get; set; }
+        public string? OssrDbmsResourceUrl { get; set; }
 
         public static async Task<AzureCloudConfig> CreateAsync(string azureCloudMetadataUrl = defaultAzureCloudMetadataUrl)
         {
+            // It's critical that this succeeds for TES to function
+            // Retry every minute for up to 10 minutes
+            // These URLs are expected to always be available
+            var retryPolicy = Policy
+                .Handle<Exception>()
+                .WaitAndRetryAsync(10, retryAttempt => TimeSpan.FromMinutes(1), onRetry: (outcome, timespan, retryAttempt, context) =>
+                {
+                    Console.WriteLine(context.CorrelationId);
+                });
+
             using var httpClient = new HttpClient();
 
-            try
+            return await retryPolicy.ExecuteAsync(async () =>
             {
-                var response = await httpClient.GetAsync(azureCloudMetadataUrl);
-                response.EnsureSuccessStatusCode();
-                var jsonString = await response.Content.ReadAsStringAsync();
-                return JsonSerializer.Deserialize<AzureCloudConfig>(jsonString)!;
-            }
-            catch (HttpRequestException e)
-            {
-                throw new Exception("Error getting AzureCloudConfig", e);
-            }
-            catch (JsonException e)
-            {
-                throw new Exception("Error deserializing JSON data for AzureCloudConfig", e);
-            }
+                var httpResponse = await httpClient.GetAsync(azureCloudMetadataUrl);
+                httpResponse.EnsureSuccessStatusCode();
+                var jsonString = await httpResponse.Content.ReadAsStringAsync();
+                var config = JsonSerializer.Deserialize<AzureCloudConfig>(jsonString)!;
+                return config;
+            });
         }
     }
 
@@ -89,48 +93,46 @@ namespace CommonUtilities.AzureCloud
         public string LoginEndpointUrl { get; set; } = "https://login.microsoftonline.com";
 
         [JsonPropertyName("audiences")]
-        public List<string> Audiences { get; set; }
+        public List<string>? Audiences { get; set; }
 
         [JsonPropertyName("tenant")]
-        public string Tenant { get; set; }
+        public string? Tenant { get; set; }
 
         [JsonPropertyName("identityProvider")]
-        public string IdentityProvider { get; set; }
+        public string? IdentityProvider { get; set; }
     }
 
     public class EndpointSuffixes
     {
         [JsonPropertyName("acrLoginServer")]
-        public string AcrLoginServerSuffix { get; set; }
+        public string? AcrLoginServerSuffix { get; set; }
 
         [JsonPropertyName("sqlServerHostname")]
-        public string SqlServerHostnameSuffix { get; set; }
+        public string? SqlServerHostnameSuffix { get; set; }
 
         [JsonPropertyName("keyVaultDns")]
-        public string KeyVaultDnsSuffix { get; set; }
+        public string? KeyVaultDnsSuffix { get; set; }
 
         [JsonPropertyName("storage")]
-        public string StorageSuffix { get; set; }
+        public string? StorageSuffix { get; set; }
 
         [JsonPropertyName("storageSyncEndpointSuffix")]
-        public string StorageSyncEndpointSuffix { get; set; }
+        public string? StorageSyncEndpointSuffix { get; set; }
 
         [JsonPropertyName("mhsmDns")]
-        public string ManagedHsmDnsSuffix { get; set; }
+        public string? ManagedHsmDnsSuffix { get; set; }
 
         [JsonPropertyName("mysqlServerEndpoint")]
-        public string MysqlServerEndpointSuffix { get; set; }
+        public string? MysqlServerEndpointSuffix { get; set; }
 
         [JsonPropertyName("postgresqlServerEndpoint")]
-        public string PostgresqlServerEndpointSuffix { get; set; }
+        public string? PostgresqlServerEndpointSuffix { get; set; }
 
         [JsonPropertyName("mariadbServerEndpoint")]
-        public string MariadbServerEndpointSuffix { get; set; }
+        public string? MariadbServerEndpointSuffix { get; set; }
 
         [JsonPropertyName("synapseAnalytics")]
-        public string SynapseAnalyticsSuffix { get; set; }
+        public string? SynapseAnalyticsSuffix { get; set; }
     }
-
-
-
 }
+
