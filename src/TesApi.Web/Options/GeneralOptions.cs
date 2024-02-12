7a4bc102431a113a74e4ef2b38581f32f4a64193
Matt McLoughlin <mattmcl@microsoft.com>
2024-02-12 11:10:05 -0800

    add comments

--- src/TesApi.Web/Options/GeneralOptions.cs
+++ src/TesApi.Web/Options/GeneralOptions.cs
@@ -3,10 +3,19 @@
 
 namespace TesApi.Web.Options
 {
+    /// <summary>
+    /// General TES system options
+    /// </summary>
     public class GeneralOptions
     {
+        /// <summary>
+        /// General options configuration section name
+        /// </summary>
         public const string SectionName = "General";
-        public string AzureCloudName { get; set; } = "AzureCloud";
+
+        /// <summary>
+        /// URL to obtain Azure cloud metadata at runtime
+        /// </summary>
         public string AzureCloudManagementUrl { get; set; } = "https://management.azure.com/metadata/endpoints?api-version=2023-11-01";
     }
 }
