7414171fac7a0bfd383f928e627032ea8828cd81
Matt McLoughlin <mattmcl@microsoft.com>
2024-02-29 19:24:45 -0800

    update script

---
+++ src/deploy-tes-docker-image.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+
+# This script builds a new TES image, pushes it the ACR, gives AKS AcrPull on the ACR, and updates the TES deployment
+
+if [ -z "$RESOURCE_GROUP_NAME" ] || [ -z "$ACR_NAME" ]; then
+    echo "Usage: $0 <ResourceGroupName> <AcrName>"
+    exit 1
+fi
+
+RESOURCE_GROUP_NAME=$1
+ACR_NAME=$2
+IMAGE_NAME=tes
+DOCKERFILE=Dockerfile-Tes
+TAG=latest
+ACR_LOGIN_SERVER="${ACR_NAME}.azurecr.io"
+NEW_IMAGE="${ACR_LOGIN_SERVER}/${IMAGE_NAME}:${TAG}"
+az login
+az acr login --name $ACR_NAME
+docker build -t $NEW_IMAGE -f $DOCKERFILE .
+echo "Pushing image... ${NEW_IMAGE}"
+docker push $NEW_IMAGE
+
+# Get the first AKS cluster name in the specified resource group
+AKS_CLUSTER_NAME=$(az aks list --resource-group $RESOURCE_GROUP_NAME --query '[0].name' -o tsv)
+if [ -z "$AKS_CLUSTER_NAME" ]; then
+    echo "No AKS cluster found in resource group $RESOURCE_GROUP_NAME."
+    exit 1
+fi
+
+echo "Found AKS Cluster: $AKS_CLUSTER_NAME"
+
+# Get the managed identity client ID used by the AKS cluster
+AKS_IDENTITY_CLIENT_ID=$(az aks show \
+    --resource-group $RESOURCE_GROUP_NAME \
+    --name $AKS_CLUSTER_NAME \
+    --query identityProfile.kubeletidentity.clientId \
+    -o tsv)
+
+# Get the ACR resource ID
+ACR_RESOURCE_ID=$(az acr show \
+    --name $ACR_NAME \
+    --resource-group $RESOURCE_GROUP_NAME \
+    --query id \
+    -o tsv)
+
+# Check if the AcrPull role assignment already exists
+EXISTING_ASSIGNMENT=$(az role assignment list \
+    --assignee $AKS_IDENTITY_CLIENT_ID \
+    --role acrpull \
+    --scope $ACR_RESOURCE_ID \
+    --query [].id \
+    -o tsv)
+
+if [ -z "$EXISTING_ASSIGNMENT" ]; then
+    # Assign AcrPull role to the AKS cluster's managed identity for the ACR
+    echo "Assigning AcrPull role to AKS..."
+    az role assignment create \
+        --assignee $AKS_IDENTITY_CLIENT_ID \
+        --role acrpull \
+        --scope $ACR_RESOURCE_ID
+    echo "AcrPull role assigned to the AKS cluster successfully."
+else
+    echo "AcrPull role assignment already exists. No action required."
+fi
+
+# Update the AKS cluster
+kubectl set image deployment/tes tes=$NEW_IMAGE -n tes
