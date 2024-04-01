3ff508f4c5fa9b3c6b61efce9b3a76b09a9c8b65
giventocode <3589801+giventocode@users.noreply.github.com>
2024-04-01 11:55:21 -0400

    tests and fixes

--- src/Tes.Runner.Test/Storage/CombinedTransformationStrategyTests.cs
+++ src/Tes.Runner.Test/Storage/CombinedTransformationStrategyTests.cs
@@ -48,5 +48,33 @@ namespace Tes.Runner.Test.Storage
 
             Assert.AreEqual(secondTransformation, result.ToString());
         }
+
+        [TestMethod]
+        public async Task CombinedTransformationStrategy_InsertsStrategyFirst_InsertedStrategyIsApplied()
+        {
+            var sourceUrl = "https://source.foo/";
+            var firstTransformation = "https://first.foo/";
+            var secondTransformation = "https://second.foo/";
+            var thirdTransformation = "https://third.foo/";
+
+            mockStrategy1.Setup(s => s.TransformUrlWithStrategyAsync(firstTransformation, It.IsAny<BlobSasPermissions>()))
+                .ReturnsAsync(() => new Uri(secondTransformation));
+            mockStrategy2.Setup(s => s.TransformUrlWithStrategyAsync(secondTransformation, It.IsAny<BlobSasPermissions>()))
+                .ReturnsAsync(() => new Uri(thirdTransformation));
+
+            var mockStrategy3 = new Mock<IUrlTransformationStrategy>();
+            mockStrategy3.Setup(s => s.TransformUrlWithStrategyAsync(sourceUrl, It.IsAny<BlobSasPermissions>()))
+                .ReturnsAsync(() => new Uri(firstTransformation));
+
+            strategy.InsertStrategy(0, mockStrategy3.Object);
+
+            var result = await strategy.TransformUrlWithStrategyAsync(sourceUrl, new BlobSasPermissions());
+
+            mockStrategy3.Verify(s => s.TransformUrlWithStrategyAsync(sourceUrl, It.IsAny<BlobSasPermissions>()), Times.Once);
+            mockStrategy1.Verify(s => s.TransformUrlWithStrategyAsync(firstTransformation, It.IsAny<BlobSasPermissions>()), Times.Once);
+            mockStrategy2.Verify(s => s.TransformUrlWithStrategyAsync(secondTransformation, It.IsAny<BlobSasPermissions>()), Times.Once);
+
+            Assert.AreEqual(thirdTransformation, result.ToString());
+        }
     }
 }
