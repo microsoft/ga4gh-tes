ce04b911434f4fb8a10983448bdda0125632440d
Blair L Murri <BMurri@users.noreply.github.com>
2024-04-05 13:43:14 -0700

    Fix syntax

--- src/TesApi.Web/Startup.cs
+++ src/TesApi.Web/Startup.cs
@@ -98,7 +98,7 @@ namespace TesApi.Web
                         {
                             opts.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
                             opts.SerializerSettings.Converters.Add(new StringEnumConverter(new CamelCaseNamingStrategy()));
-                            opts.SerializerSettings.DefaultValueHandling = DefaultValueHandling.Ignore;
+                            opts.SerializerSettings.DefaultValueHandling = Newtonsoft.Json.DefaultValueHandling.Ignore;
                         })
                     .Services
                     .AddSingleton(CreateStorageAccessProviderFromConfiguration)
