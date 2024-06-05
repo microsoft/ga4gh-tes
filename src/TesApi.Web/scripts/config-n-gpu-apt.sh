539a16cde99114f8cb196996af3141abe1781767
Blair L Murri <BMurri@users.noreply.github.com>
2024-06-05 06:35:31 -0700

    Add link to NVIDIA docs

--- src/TesApi.Web/scripts/config-n-gpu-apt.sh
+++ src/TesApi.Web/scripts/config-n-gpu-apt.sh
@@ -2,6 +2,8 @@
 trap "echo Error trapped; exit 0" ERR
 # set -e will cause any error to exit the script
 set -e
+
+# https://docs.nvidia.com/datacenter/cloud-native/container-toolkit/latest/install-guide.html
 curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | sudo gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg \
   && curl -s -L https://nvidia.github.io/libnvidia-container/stable/deb/nvidia-container-toolkit.list | \
     sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | \
--- src/TesApi.Web/scripts/config-n-gpu-yum.sh
+++ src/TesApi.Web/scripts/config-n-gpu-yum.sh
@@ -2,6 +2,8 @@
 trap "echo Error trapped; exit 0" ERR
 # set -e will cause any error to exit the script
 set -e
+
+# https://docs.nvidia.com/datacenter/cloud-native/container-toolkit/latest/install-guide.html
 curl -s -L https://nvidia.github.io/libnvidia-container/stable/rpm/nvidia-container-toolkit.repo | \
   sudo tee /etc/yum.repos.d/nvidia-container-toolkit.repo
 
