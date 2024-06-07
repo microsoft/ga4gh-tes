77b98983d0277259d5afdf33ab0666c502eb7e21
Blair L Murri <BMurri@users.noreply.github.com>
2024-06-07 14:48:09 -0700

    small refactor

--- src/Tes.RunnerCLI/Services.cs
+++ src/Tes.RunnerCLI/Services.cs
@@ -13,18 +13,19 @@ using Tes.Runner.Events;
 using Tes.Runner.Logs;
 using Tes.Runner.Storage;
 using Tes.Runner.Transfer;
+using Tes.RunnerCLI.Commands;
 
-namespace Tes.RunnerCLI.Commands
+namespace Tes.RunnerCLI
 {
     public class Services
     {
         public const string LogLevelEnvVariableName = "RUNNER_LOG_LEVEL";
 
-        internal static void ConfigureConsoleLogger(ILoggingBuilder builder)
+        private static void ConfigureConsoleLogger(ILoggingBuilder builder)
         {
             builder.AddFilter("Microsoft.Hosting.Lifetime", LogLevel.Error);
 
-            var logLevel = LogLevel.Information;
+            var logLevel = LogLevel.Information; // default minimum log level for the console logger
 
             if (Enum.TryParse<LogLevel>(Environment.GetEnvironmentVariable(LogLevelEnvVariableName), out var userLevel))
             {
@@ -36,20 +37,50 @@ namespace Tes.RunnerCLI.Commands
                 options.IncludeScopes = true;
                 options.TimestampFormat = "yyyy-MM-dd HH:mm:ss.fff ";
                 options.UseUtcTimestamp = true;
-            }).SetMinimumLevel(logLevel);
+                options.ColorBehavior = Microsoft.Extensions.Logging.Console.LoggerColorBehavior.Default;
+                //options.SingleLine = true;
+            })
+                .SetMinimumLevel(logLevel);
         }
 
+        #region ConfigureParameters
         private static void ConfigureParametersCore(IHostApplicationBuilder builder, Runner.Models.RuntimeOptions runtimeOptions, string apiVersion)
-            => builder.Services
-                .AddSingleton(runtimeOptions)
-                .AddKeyedSingleton(Executor.ApiVersion, apiVersion)
-                .AddSingleton(runtimeOptions.Terra ?? new())
-                .AddSingleton(runtimeOptions.AzureEnvironmentConfig ?? new())
-            ;
+        {
+            builder.Services
+                    .AddSingleton(runtimeOptions)
+                    .AddKeyedSingleton(Executor.ApiVersion, apiVersion)
+                    .AddSingleton(runtimeOptions.Terra!)
+                    .AddSingleton(runtimeOptions.AzureEnvironmentConfig ?? GetDefaultEnvironmentConfig())
+                ;
 
+            static CommonUtilities.AzureEnvironmentConfig GetDefaultEnvironmentConfig()
+            {
+                var env = Microsoft.Azure.Management.ResourceManager.Fluent.AzureEnvironment.AzureGlobalCloud;
+                return new()
+                {
+                    AzureAuthorityHostUrl = env.AuthenticationEndpoint,
+                    StorageUrlSuffix = env.StorageEndpointSuffix,
+                    TokenScope = Azure.ResourceManager.ArmEnvironment.AzurePublicCloud.DefaultScope
+                };
+            }
+        }
+
+
+        /// <summary>
+        /// Configures services using the <paramref name="runtimeOptions"/>.
+        /// </summary>
+        /// <param name="nodeTask">The node task.</param>
+        /// <param name="apiVersion">The blob storage API version.</param>
+        /// <returns>A services configuration method.</returns>
         internal static Action<IHostApplicationBuilder> ConfigureParameters(Runner.Models.RuntimeOptions runtimeOptions, string apiVersion)
             => new(builder => ConfigureParametersCore(builder, runtimeOptions, apiVersion));
 
+        /// <summary>
+        /// Configures services using the <paramref name="nodeTask"/>.
+        /// </summary>
+        /// <param name="nodeTask">The node task.</param>
+        /// <param name="apiVersion">The blob storage API version.</param>
+        /// <returns>A services configuration method.</returns>
         internal static Action<IHostApplicationBuilder> ConfigureParameters(Runner.Models.NodeTask nodeTask, string apiVersion)
             => new(builder =>
             {
@@ -59,10 +90,25 @@ namespace Tes.RunnerCLI.Commands
 
                 ConfigureParametersCore(builder, nodeTask.RuntimeOptions, apiVersion);
             });
+        #endregion
 
+        /// <summary>
+        /// Creates a <typeparamref name="T"/> using <paramref name="factory"/>.
+        /// </summary>
+        /// <typeparam name="T">The type to create.</typeparam>
+        /// <param name="factory">A factory method accepting an argument of type <see cref="ILogger{TCategoryName}"/> where <c>TCategoryName</c> is <typeparamref name="T"/>.</param>
+        /// <returns>An instance of <typeparamref name="T"/>.</returns>
         internal static T Create<T>(Func<ILogger<T>, T> factory)
             => factory(LoggerFactory.Create(ConfigureConsoleLogger).CreateLogger<T>());
 
+        /// <summary>
+        /// Builds a <typeparamref name="TService"/> and runs <paramref name="task"/>.
+        /// </summary>
+        /// <typeparam name="TService">The type of the service.</typeparam>
+        /// <typeparam name="TResult">The type of the result.</typeparam>
+        /// <param name="task">Method taking <typeparamref name="TService"/> and returning <typeparamref name="TResult"/>.</param>
+        /// <param name="configure">Configuration method for <typeparamref name="TService"/>.</param>
+        /// <returns>The result of calling <paramref name="task"/>.</returns>
         internal static Task<TResult> BuildAndRunAsync<TService, TResult>(Func<TService, Task<TResult>> task, Action<IHostApplicationBuilder>? configure = default) where TService : notnull
             => BuildAndRunImplAsync(provider => task(provider.GetRequiredService<TService>()), configure);
 
@@ -86,6 +132,7 @@ namespace Tes.RunnerCLI.Commands
             }
         }
 
+        #region Service factory helpers
         private static Func<IServiceProvider, Lazy<T>> LazyFactory<T>(Func<IServiceProvider, T> factory, LazyThreadSafetyMode mode)
             => provider => new Lazy<T>(() => factory(provider), mode);
 
@@ -100,10 +147,12 @@ namespace Tes.RunnerCLI.Commands
 
         private static Func<IServiceProvider, Func<TArgA, TArgB, TArgC, TArgD, TService>> GetFactory<TService, TArgA, TArgB, TArgC, TArgD>() =>
             new(provider => new((argA, argB, argC, argD) => ActivatorUtilities.CreateFactory<TService>([typeof(TArgA), typeof(TArgB), typeof(TArgC), typeof(TArgD)])(provider, [argA, argB, argC, argD])));
+        #endregion
 
-        private static void Configure(IServiceCollection services, IConfigurationManager configuration)
+        private static void Configure(IServiceCollection services, IConfigurationManager _/*configuration*/)
         {
             services
+                // Methods to acquire instance of services using arguments
                 .AddSingleton(GetFactory<AppendBlobLogPublisher, Uri, string>())
                 .AddSingleton(GetFactory<BlobDownloader, BlobPipelineOptions, Channel<byte[]>>())
                 .AddSingleton(GetFactory<BlobStorageEventSink, Uri>())
@@ -116,6 +165,7 @@ namespace Tes.RunnerCLI.Commands
                 .AddSingleton(GetFactory<ProcessedPartsProcessor, IBlobPipeline>())
                 .AddSingleton(GetFactory<ProcessLauncher, IStreamLogReader>())
 
+                // Services that depend on other services or configuration in specific ways
                 .AddSingleton<Func<Runner.Models.RuntimeOptions, string, ResolutionPolicyHandler>>(provider =>
                     new((runtime, version) => ActivatorUtilities.CreateFactory<ResolutionPolicyHandler>([typeof(UrlTransformationStrategyFactory)])
                         .Invoke(provider, [ActivatorUtilities.CreateFactory<UrlTransformationStrategyFactory>([typeof(Runner.Models.RuntimeOptions), typeof(string)])
@@ -125,21 +175,7 @@ namespace Tes.RunnerCLI.Commands
                 .AddSingleton<Func<Runner.Models.RuntimeOptions, string, Azure.Core.TokenCredential>>(provider =>
                     new((runtimeOptions, tokenScope) => provider.GetRequiredService<CredentialsManager>().GetTokenCredential(runtimeOptions, tokenScope)))
 
-                .AddSingleton<CommandLauncher>()
-                .AddSingleton<CommandHandlers>()
-                .AddSingleton<ContainerRegistryAuthorizationManager>()
-                .AddSingleton<CredentialsManager>()
-                .AddSingleton<Executor>()
-                .AddSingleton<IFileInfoProvider, DefaultFileInfoProvider>()
-                .AddSingleton<FileOperationResolver>()
-                .AddSingleton<LogPublisher>()
-                .AddSingleton<NetworkUtility>()
-                .AddSingleton<ProcessLauncherFactory>()
-                .AddSingleton<ISystemInfoProvider, LinuxSystemInfoProvider>()
-                .AddSingleton<ITransferOperationFactory, TransferOperationFactory>()
-                .AddSingleton<UrlTransformationStrategyFactory>()
-                .AddSingleton<VolumeBindingsGenerator>()
-
+                // Services that are lazily constructed
                 .AddSingleton(LazyFactory(provider =>
                     provider.GetRequiredService<ConsoleStreamLogPublisher>(),
                     LazyThreadSafetyMode.PublicationOnly))
@@ -160,26 +196,47 @@ namespace Tes.RunnerCLI.Commands
                     LazyThreadSafetyMode.PublicationOnly))
                 .AddTransient<PipelineOptionsOptimizer>()
 
+                // Keyed services
                 .AddKeyedSingleton(UrlTransformationStrategyFactory.CloudProvider, LazyKeyedFactory(provider =>
                     (IUrlTransformationStrategy)provider.GetRequiredService<CloudProviderSchemeConverter>(),
                     LazyThreadSafetyMode.PublicationOnly))
                 .AddTransient<CloudProviderSchemeConverter>()
+
                 .AddKeyedSingleton(UrlTransformationStrategyFactory.PassThroughUrl, LazyKeyedFactory(provider =>
                     (IUrlTransformationStrategy)provider.GetRequiredService<PassThroughUrlTransformationStrategy>(),
                     LazyThreadSafetyMode.PublicationOnly))
                 .AddTransient<PassThroughUrlTransformationStrategy>()
+
                 .AddKeyedSingleton(UrlTransformationStrategyFactory.ArmUrl, LazyKeyedFactory(provider =>
                     (IUrlTransformationStrategy)provider.GetRequiredService<ArmUrlTransformationStrategy>(),
                     LazyThreadSafetyMode.PublicationOnly))
                 .AddTransient<ArmUrlTransformationStrategy>()
+
                 .AddKeyedSingleton(UrlTransformationStrategyFactory.TerraUrl, LazyKeyedFactory(provider =>
                     (IUrlTransformationStrategy)provider.GetRequiredService<TerraUrlTransformationStrategy>(),
                     LazyThreadSafetyMode.PublicationOnly))
                 .AddTransient<TerraUrlTransformationStrategy>()
+
                 .AddKeyedSingleton(UrlTransformationStrategyFactory.DrsUrl, LazyKeyedFactory(provider =>
                     (IUrlTransformationStrategy)provider.GetRequiredService<DrsUriTransformationStrategy>(),
                     LazyThreadSafetyMode.PublicationOnly))
                 .AddTransient<DrsUriTransformationStrategy>()
+
+                // Other services
+                .AddSingleton<CommandLauncher>()
+                .AddSingleton<CommandHandlers>()
+                .AddSingleton<ContainerRegistryAuthorizationManager>()
+                .AddSingleton<CredentialsManager>()
+                .AddSingleton<Executor>()
+                .AddSingleton<IFileInfoProvider, DefaultFileInfoProvider>()
+                .AddSingleton<FileOperationResolver>()
+                .AddSingleton<LogPublisher>()
+                .AddSingleton<NetworkUtility>()
+                .AddSingleton<ProcessLauncherFactory>()
+                .AddSingleton<ISystemInfoProvider, LinuxSystemInfoProvider>()
+                .AddSingleton<ITransferOperationFactory, TransferOperationFactory>()
+                .AddSingleton<UrlTransformationStrategyFactory>()
+                .AddSingleton<VolumeBindingsGenerator>()
                 ;
         }
     }
