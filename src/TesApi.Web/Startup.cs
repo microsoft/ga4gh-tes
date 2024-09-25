2e4f4f9f57de797f75c882ded705c41fe7345f6f
Blair L Murri <BMurri@users.noreply.github.com>
2024-09-24 17:00:22 -0700

    Update version to 5.4.3 (#775)

--- src/TesApi.Web/Startup.cs
+++ src/TesApi.Web/Startup.cs
@@ -42,7 +42,7 @@ namespace TesApi.Web
     public class Startup
     {
         // TODO centralize in single location
-        internal const string TesVersion = "5.4.2";
+        internal const string TesVersion = "5.4.3";
         private readonly IConfiguration configuration;
         private readonly ILogger logger;
         private readonly IWebHostEnvironment hostingEnvironment;
