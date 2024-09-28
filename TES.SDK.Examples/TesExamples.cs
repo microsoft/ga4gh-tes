f3b79073577aa1c20774ea2cc555cf8b5b71993b
Matt McLoughlin <mattmcl@microsoft.com>
2024-09-28 12:32:44 -0700

    output primes and download results

--- TES.SDK.Examples/TesExamples.cs
+++ TES.SDK.Examples/TesExamples.cs
@@ -1,6 +1,8 @@
 ﻿// Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
+using Azure.Identity;
+using Azure.Storage.Blobs;
 using Tes.Models;
 using Tes.SDK;
 
@@ -24,28 +26,36 @@ namespace TES.SDK.Examples
             task.Resources.CpuCores = 1;
             task.Resources.RamGb = 1;
             //testTesTask.Resources.DiskGb = 100;
-
+            const long maxPrimes = 1_000_000;
+            string outputFileName = $"primes-2-{maxPrimes}.txt";
             task.Executors.Add(new()
             {
                 Image = "ubuntu:22.04",
                 Command = [ 
                     "/bin/sh",
                     "-c",
-                    "chmod 1777 /tmp && apt-get update && apt-get install -y primesieve && primesieve 1e6 > /tmp/output.txt"
+                    $"chmod 1777 /tmp && apt-get update && apt-get install -y primesieve && primesieve {maxPrimes} -p > /tmp/{outputFileName}"
                 ]
             });
 
             task.Outputs.Add(new()
             {
-                Path = "/tmp/output.txt",
-                Url = $"https://{_storageAccountName}.blob.core.windows.net/outputs/output.txt"
+                Path = $"/tmp/{outputFileName}",
+                Url = $"https://{_storageAccountName}.blob.core.windows.net/outputs/{outputFileName}"
             });
             
             using ITesClient tesClient = new TesClient(_tesCredentials);
             var completedTask = await tesClient.CreateAndWaitTilDoneAsync(task);
             Console.WriteLine($"TES Task State: {completedTask.State}");
 
-            if (completedTask.State != TesState.COMPLETE)
+            if (completedTask.State == TesState.COMPLETE)
+            {
+                var client = new BlobClient(new Uri(task.Outputs.First().Url), new DefaultAzureCredential());
+                var downloadResponse = await client.DownloadContentAsync(CancellationToken.None);
+                var output = downloadResponse.Value.Content.ToString();
+                Console.WriteLine(output);
+            }
+            else
             {
                 Console.WriteLine($"Failure reason: {completedTask.FailureReason}");
                 var logs = await tesClient.DownloadLogsAsync(completedTask, _storageAccountName, CancellationToken.None);
