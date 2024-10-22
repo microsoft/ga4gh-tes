76eda2f7f102192cde06a5b8f3ed86f755fab2ce
Matt McLoughlin <mattmcl@microsoft.com>
2024-10-22 14:47:35 -0700

    remove whitespace

--- TES.SDK.Examples/TesExamples.cs
+++ TES.SDK.Examples/TesExamples.cs
@@ -77,8 +77,7 @@ namespace TES.SDK.Examples
                     new TesExecutor
                     {
                         Image = "quay.io/biocontainers/bwa:0.7.18--he4a0461_1",
-                        Command = new List<string> 
-                        {
+                        Command = new List<string> {
                             "/bin/sh", 
                             "-c",
                             $"bwa index /data/Homo_sapiens_assembly38.fasta && bwa mem -t 16 /data/Homo_sapiens_assembly38.fasta /data/H06HDADXX130110.1.ATCACGAT.20k_reads_1.fastq /data/H06HDADXX130110.1.ATCACGAT.20k_reads_2.fastq > /data/{outputFileName}"
